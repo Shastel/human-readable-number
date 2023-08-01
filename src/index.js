f4bed64eda87c5e929310df08379b3dbcd7b1b38
Bodnar Alex <allugovskova@mail.ru>
2023-08-01 15:02:09 +0200

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function toReadable (number) {
-  
+
+    let lessTen = ["zero" , "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    let moreTen = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    let tens = ["ten","twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety", "hundred"];
+
+    number = String(number);
+    let res ='';
+
+    if(number.length === 3){
+        res = lessTen[number[0]] + " hundred ";
+        number = number.slice(1);
+    }
+    if(number.length === 2){
+        if(number[0] == 1){
+            res += moreTen[number[1]] + " ";
+            number = [];
+            
+        }else if(number[0] == 0){
+            number = number.slice(1);
+        }else{
+            res += tens[number[0]-1] + " ";
+            number = number.slice(1);
+        }
+    }
+    if(number.length === 1){
+        if(number != 0){
+            res += lessTen[number[0]] + " ";
+        }else if(!res){
+            res = lessTen[number[0]] + " ";
+        }
+    }
+
+    res = res.slice(0, res.length - 1);
+    return res;
 }
